324a305ad8f9471081702c93830bbdf3e21265e4
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-08-23 15:52:38 +0800

    feat: 4.1.28

--- yunzai.config.js
+++ yunzai.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'yunzai'
 export default defineConfig({
   applications: ['yz-system'],
-  middlewares: ['yz-yz-mw-runtime', 'yunzai-mys/mw']
+  middlewares: ['yz-mw-runtime', 'yunzai-mys/mw']
 })
